9d7f34085e59ec5823bfbcbb6fdf97311d71ce41
Meviii <misaluk20@gmail.com>
2022-04-12 05:27:12 +1000

    starter_code

---
+++ scrabble.cpp
@@ -0,0 +1,14 @@
+
+#include "LinkedList.h"
+
+#include <iostream>
+
+#define EXIT_SUCCESS    0
+
+int main(int argc, char* args[]) {
+   LinkedList* list = new LinkedList();
+   delete list;
+
+
+   return EXIT_SUCCESS;
+}
